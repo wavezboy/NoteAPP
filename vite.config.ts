2ffbde3ae2ad0a34191d7c6b58673e5f6c90c341
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-05-27 08:09:42 +0100

    fix proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:5000",
-        target: "https://noteappbackend-production-94d1.up.railway.app",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       // target: "http://localhost:5000",
+  //       target: "https://noteappbackend-production-94d1.up.railway.app",
+  //       changeOrigin: true,
+  //       secure: true,
+  //     },
+  //   },
+  // },
 });
